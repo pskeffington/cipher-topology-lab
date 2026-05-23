aec438e2fbdd020d37615d6ea0c9984546949a2a
Paul Skeffington <36854171+pskeffington@users.noreply.github.com>
2026-05-23 03:00:26 -0400

    Add embedding tests

---
+++ tests/test_embeddings.py
@@ -0,0 +1,13 @@
+from ciphertopology.embeddings import byte_pair_embedding, sliding_window_embedding
+
+
+def test_byte_pair_embedding_shape():
+    data = bytes(range(10))
+    pts = byte_pair_embedding(data)
+    assert pts.shape == (5, 2)
+
+
+def test_sliding_window_embedding_shape():
+    data = bytes(range(16))
+    pts = sliding_window_embedding(data, dimension=4, stride=2)
+    assert pts.shape == (7, 4)
